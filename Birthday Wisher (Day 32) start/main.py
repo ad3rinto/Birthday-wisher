0495e0ee4f43961d85b7e117ae20a4e6afddf6b9
ad3rinto <aad3rinto@gmail.com>
2024-05-29 12:20:07 +0100

    done

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,10 +1,23 @@
+import random
 import smtplib
-import key
-from key import PASSWORD
+# import key
+import datetime as dt
 
-my_email = "mojojo.aderinto@gmail.com"
-my_password = key.PASSWORD
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="agbako@myyahoo.com", msg="Hello again")
+# from key import PASSWORD
+day_of_week = dt.datetime.now()
+with open("quotes.txt") as quote_list:
+    content = quote_list.readlines()
+    print(random.choice(content))
+    print(day_of_week)
+
+if day_of_week.weekday() == 1
+    my_email = "mojojo.aderinto@gmail.com"
+    my_password = key.PASSWORD
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(from_addr=my_email, to_addrs="agbako@myyahoo.com", msg="Hello again")
+
+
+
+
